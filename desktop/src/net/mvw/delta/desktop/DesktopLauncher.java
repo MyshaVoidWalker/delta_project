45ab506eddc8bc389f41746226fc1029a58bd485
Mysha <mistermysha@gmail.com>
2019-11-22 11:41:48 +0200

    Inital Setup.

---
+++ desktop/src/net/mvw/delta/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mvw.delta.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mvw.delta.ProjectDelta;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectDelta(), config);
+	}
+}
